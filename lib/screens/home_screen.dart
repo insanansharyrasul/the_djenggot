348d8740242c8b183ef238472d3d44e7535ec941
insanansharyrasul <insanansryrasul21@gmail.com>
2025-03-29 15:19:49 +0700

    feat - changing home screen and stock screen, also removed welcomeview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:the_djenggot/screens/stock_screen.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:the_djenggot/utils/theme/app_theme.dart';
+import 'package:the_djenggot/widgets/sales_today.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => const StockScreen(),
-            ));
-          },
-          child: const Text("Go to Stock"))
-    ],);
+    return ListView(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Icon(Iconsax.menu),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  "Warung Djenggot",
+                  style: TextStyle(
+                    fontFamily: DjenggotAppTheme.fontName,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    letterSpacing: 0.5,
+                    color: Colors.black, // Replace with your theme color
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Icon(Iconsax.notification4),
+              ),
+            ],
+          ),
+        ),
+        SalesToday()
+      ],
+    );
   }
 }
--- lib/screens/stock_screen.dart
+++ lib/screens/stock_screen.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:the_djenggot/utils/theme/app_theme.dart';
 
-class StockScreen extends StatelessWidget {
+class StockScreen extends StatefulWidget {
   const StockScreen({super.key});
 
   @override
+  State<StockScreen> createState() => _StockScreenState();
+}
+
+class _StockScreenState extends State<StockScreen> {
+  TextEditingController search = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Stock"),
-            ElevatedButton(onPressed: () => context.pop(), child: Text("Back"))
-          ],
+    return ListView(
+      children: [
+        Text(
+          "Stok",
+          style: TextStyle(
+            fontFamily: DjenggotAppTheme.fontName,
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            letterSpacing: 0.5,
+            color: Colors.black, // Replace with your theme color
+          ),
         ),
-      ),
+        const SizedBox(height: 16),
+
+        
+      ],
     );
   }
 }
