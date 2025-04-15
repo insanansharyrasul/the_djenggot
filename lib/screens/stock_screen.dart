fb4af6e13f726bba1f4b7952e76330a36ff500e5
insanansharyrasul <insanansryrasul21@gmail.com>
2025-04-15 12:29:35 +0700

    fix - added refresh indicator for stock screen

--- lib/screens/stock_screen.dart
+++ lib/screens/stock_screen.dart
@@ -29,142 +29,149 @@ class _StockScreenState extends State<StockScreen> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: [
-          Text(
-            "Stok",
-            style: TextStyle(
-              fontFamily: AppTheme.fontName,
-              fontWeight: FontWeight.w500,
-              fontSize: 18,
-              letterSpacing: 0.5,
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(height: 16),
-          Row(
-            children: [
-              Expanded(child: SearchBar(
-                controller: search,
-                elevation: WidgetStateProperty.all(0.0),
-                backgroundColor: WidgetStateProperty.all(AppTheme.white),
-                hintText: "Cari Stok",
-                shape: WidgetStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    side: const BorderSide(color: AppTheme.white),
-                  ),
-                ),
-                onChanged: (value) {
-                  BlocProvider.of<StockBloc>(context).add(
-                    SearchStock(value),
-                  );
-                },
-              )),
-              PopupMenuButton(
-                icon: Icon(Iconsax.sort),
-                color:  AppTheme.white,
-                itemBuilder: (context) => [
-                  PopupMenuItem(
-                    value: "name",
-                    child: Text("Nama Stok", style: AppTheme.textField),
-                  ),
-                  PopupMenuItem(
-                    value: "quantity",
-                    child: Text("Kuantitas", style: AppTheme.textField),
-                  ),
-                ],
-                iconSize: 24,
-                onSelected: (value) {
-                  setState(() {
-                    sortBy = value;
-                  });
-                },
+      child: RefreshIndicator(
+        onRefresh: () async {
+          BlocProvider.of<StockBloc>(context).add(LoadStock());
+        },
+        child: ListView(
+          children: [
+            Text(
+              "Stok",
+              style: TextStyle(
+                fontFamily: AppTheme.fontName,
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                letterSpacing: 0.5,
+                color: Colors.black,
               ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    descending = descending == "asc" ? "desc" : "asc";
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                    child: SearchBar(
+                  controller: search,
+                  elevation: WidgetStateProperty.all(0.0),
+                  backgroundColor: WidgetStateProperty.all(AppTheme.white),
+                  hintText: "Cari Stok",
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      side: const BorderSide(color: AppTheme.white),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    BlocProvider.of<StockBloc>(context).add(
+                      SearchStock(value),
+                    );
+                  },
+                )),
+                PopupMenuButton(
+                  icon: Icon(Iconsax.sort),
+                  color: AppTheme.white,
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: "name",
+                      child: Text("Nama Stok", style: AppTheme.textField),
+                    ),
+                    PopupMenuItem(
+                      value: "quantity",
+                      child: Text("Kuantitas", style: AppTheme.textField),
+                    ),
+                  ],
+                  iconSize: 24,
+                  onSelected: (value) {
+                    setState(() {
+                      sortBy = value;
+                    });
+                  },
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      descending = descending == "asc" ? "desc" : "asc";
+                    });
+                  },
+                  child:
+                      descending == "asc" ? Icon(Icons.arrow_upward) : Icon(Icons.arrow_downward),
+                )
+              ],
+            ),
+            const SizedBox(height: 16),
+            BlocBuilder<StockBloc, StockState>(builder: (context, state) {
+              if (state is StockLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is StockLoaded) {
+                final sortedStocks = List<Stock>.from(state.stocks)
+                  ..sort((a, b) {
+                    int comparison;
+                    if (sortBy == "name") {
+                      comparison = a.name.compareTo(b.name);
+                    } else {
+                      comparison = a.quantity.compareTo(b.quantity);
+                    }
+                    return descending == "asc" ? comparison : -comparison;
                   });
-                },
-                child: descending == "asc" ? Icon(Icons.arrow_upward) : Icon(Icons.arrow_downward),
-              )
-            ],
-          ),
-          const SizedBox(height: 16),
-          BlocBuilder<StockBloc, StockState>(builder: (context, state) {
-            if (state is StockLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is StockLoaded) {
-              final sortedStocks = List<Stock>.from(state.stocks)
-                ..sort((a, b) {
-                  int comparison;
-                  if (sortBy == "name") {
-                    comparison = a.name.compareTo(b.name);
-                  } else {
-                    comparison = a.quantity.compareTo(b.quantity);
-                  }
-                  return descending == "asc" ? comparison : -comparison;
-                });
 
-              return ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: sortedStocks.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      // state.stocks[index].name,
-                      sortedStocks[index].name,
-                      style: AppTheme.textField,
-                    ),
-                    subtitle: Text(
-                      // "Kuantitas: ${state.stocks[index].quantity}",
-                      "Kuantitas: ${sortedStocks[index].quantity}",
-                      style: AppTheme.subtitle,
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        GestureDetector(
-                            onTap: () {
-                              context.push('/add-edit-stock', extra: sortedStocks[index]);
-                            },
-                            child: const Icon(Iconsax.edit)),
-                        const SizedBox(width: 16),
-                        GestureDetector(
-                            onTap: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AppDialog(
-                                    type: "confirm",
-                                    title: "Anda akan menghapus stok ini!",
-                                    message: "Apakah anda yakin?",
-                                    okTitle: "Hapus",
-                                    cancelTitle: "Batal",
-                                    onOkPress: () {
-                                      Navigator.pop(context);
-                                      BlocProvider.of<StockBloc>(context).add(
-                                        DeleteStock(state.stocks[index].id),
-                                      );
-                                    },
-                                  );
-                                },
-                              );
-                            },
-                            child: const Icon(Iconsax.trash, color: Colors.red)),
-                      ],
-                    ),
-                  );
-                },
-              );
-            }
-            return Container();
-          })
-        ],
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: sortedStocks.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        // state.stocks[index].name,
+                        sortedStocks[index].name,
+                        style: AppTheme.textField,
+                      ),
+                      subtitle: Text(
+                        // "Kuantitas: ${state.stocks[index].quantity}",
+                        "Kuantitas: ${sortedStocks[index].quantity}",
+                        style: AppTheme.subtitle,
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          GestureDetector(
+                              onTap: () {
+                                context.push('/add-edit-stock', extra: sortedStocks[index]);
+                              },
+                              child: const Icon(Iconsax.edit)),
+                          const SizedBox(width: 16),
+                          GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AppDialog(
+                                      type: "confirm",
+                                      title: "Anda akan menghapus stok ini!",
+                                      message: "Apakah anda yakin?",
+                                      okTitle: "Hapus",
+                                      cancelTitle: "Batal",
+                                      onOkPress: () {
+                                        Navigator.pop(context);
+                                        BlocProvider.of<StockBloc>(context).add(
+                                          DeleteStock(state.stocks[index].id),
+                                        );
+                                      },
+                                    );
+                                  },
+                                );
+                              },
+                              child: const Icon(Iconsax.trash, color: Colors.red)),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
+              return Container();
+            })
+          ],
+        ),
       ),
     );
   }
